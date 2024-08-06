c9314484b8bad76f691723e4aea531e11c78a883
Artem Torkit <artemtorkitdev@gmail.com>
2024-08-06 23:04:19 +0200

    added images to galery

--- client/src/assets/portfolio/index.js
+++ client/src/assets/portfolio/index.js
@@ -1,17 +1,23 @@
 import portfolioOne from './1.jpg'
 import portfolioTwo from './2.jpg'
-import portfolioThree from './3.jpg'
 import portfolioFour from './4.jpg'
 import portfolioFive from './5.jpg'
 import portfolioSix from './6.jpg'
 import portfolioSeven from './7.jpg'
+import portfolioEight from './8.jpg'
+import portfolioNine from './9.jpg'
+import portfolioTen from './10.jpg'
+import portfolioEleven from './11.jpg'
 
 export {
     portfolioOne,
     portfolioTwo,
-    portfolioThree,
     portfolioFour,
     portfolioFive,
     portfolioSix,
     portfolioSeven,
+    portfolioEight,
+    portfolioNine,
+    portfolioTen,
+    portfolioEleven
 }
--- client/src/components/Galery.jsx
+++ client/src/components/Galery.jsx
@@ -1,22 +1,28 @@
 import "react-slideshow-image/dist/styles.css";
 import { Slide } from "react-slideshow-image";
 import {
-    portfolioOne,
-    portfolioTwo,
-    portfolioThree,
-    portfolioFour,
-    portfolioFive,
-    portfolioSix,
-    portfolioSeven,
-} from '../assets/portfolio'
-const images = [
   portfolioOne,
   portfolioTwo,
-  portfolioThree,
   portfolioFour,
   portfolioFive,
   portfolioSix,
   portfolioSeven,
+  portfolioEight,
+  portfolioNine,
+  portfolioTen,
+  portfolioEleven,
+} from "../assets/portfolio";
+const images = [
+  portfolioOne,
+  portfolioTwo,
+  portfolioFour,
+  portfolioFive,
+  portfolioSix,
+  portfolioSeven,
+  portfolioEight,
+  portfolioNine,
+  portfolioTen,
+  portfolioEleven,
 ];
 
 const Galery = () => {
@@ -33,11 +39,48 @@ const Galery = () => {
               
             </div>
           </div>
+
           <div className="each-slide-effect">
-            <div style={{ backgroundImage: `url(${images[2]})` }}>
+            <div style={{ backgroundImage: `url(${images[3]})` }}>
               
             </div>
           </div>
+          <div className="each-slide-effect">
+            <div style={{ backgroundImage: `url(${images[4]})` }}>
+              
+            </div>
+          </div>
+          <div className="each-slide-effect">
+            <div style={{ backgroundImage: `url(${images[5]})` }}>
+              
+            </div>
+          </div>
+          <div className="each-slide-effect">
+            <div style={{ backgroundImage: `url(${images[6]})` }}>
+              
+            </div>
+          </div>
+          <div className="each-slide-effect">
+            <div style={{ backgroundImage: `url(${images[7]})` }}>
+              
+            </div>
+          </div>
+          <div className="each-slide-effect">
+            <div style={{ backgroundImage: `url(${images[8]})` }}>
+              
+            </div>
+          </div>
+          <div className="each-slide-effect">
+            <div style={{ backgroundImage: `url(${images[9]})` }}>
+              
+            </div>
+          </div>
+          <div className="each-slide-effect">
+            <div style={{ backgroundImage: `url(${images[10]})` }}>
+              
+            </div>
+          </div>
+
         </Slide>
       </div>
     );
